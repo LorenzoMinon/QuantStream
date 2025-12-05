5e3a751b4d8d43bf79d2652fca4aaf48027c98ea
Lorenzo Miñón <lorenzominon01@gmail.com>
2025-12-05 12:25:29 -0300

    infra: deploy s3 dlake using terraform

---
+++ infrastructure/main.tf
@@ -0,0 +1,52 @@
+provider "aws" {
+  region = "us-east-1" 
+}
+
+# 1. EL BUCKET (La caja fuerte principal)
+resource "aws_s3_bucket" "data_lake" {
+  bucket = "quantstream-lake-lorenzo-2026"
+
+  # Esto permite borrar el bucket aunque tenga cosas adentro (solo para desarrollo)
+  force_destroy = true 
+
+  tags = {
+    Name  = "QuantStream Data Lake"
+    Owner = "Lorenzo"
+  }
+}
+
+# 2. EL CANDADO (Seguridad)
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket = aws_s3_bucket.data_lake.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+# 3. LAS CARPETAS (Bronze, Silver, Gold)
+# Usamos content="" para crear una "carpeta vacía" compatible con Windows
+
+resource "aws_s3_object" "bronze" {
+  bucket  = aws_s3_bucket.data_lake.id
+  key     = "bronze/"
+  content = "" 
+}
+
+resource "aws_s3_object" "silver" {
+  bucket  = aws_s3_bucket.data_lake.id
+  key     = "silver/"
+  content = ""
+}
+
+resource "aws_s3_object" "gold" {
+  bucket  = aws_s3_bucket.data_lake.id
+  key     = "gold/"
+  content = ""
+}
+
+# 4. EL MENSAJE FINAL
+output "nombre_del_bucket" {
+  value = aws_s3_bucket.data_lake.id
+}
