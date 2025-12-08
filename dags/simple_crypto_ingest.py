5838008566b197789e85f9a49f5990969e8c9e41
Lorenzo Miñón <lorenzominon01@gmail.com>
2025-12-07 22:43:36 -0300

    feat: implement bronze to silver transformation with pandas

--- dags/simple_crypto_ingest.py
+++ dags/simple_crypto_ingest.py
@@ -5,6 +5,8 @@ from datetime import datetime
 import requests
 import json
 import logging
+import pandas as pd
+import io
 
 # config
 BUCKET_NAME = "quantstream-lake-lorenzo-2026"
@@ -15,46 +17,83 @@ default_args = {
     'retries': 1,
 }
 
-def fetch_and_save_bitcoin():
-    # 1. extract. save to json > s3
+def fetch_bitcoin_raw(**context):
+    # downloads json and returns route saved in s3
     url = "https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT"
-    try:
-        response = requests.get(url)
-        data = response.json()
-        price = data['price']
-        
-        # timestamp
-        data['ingestion_timestamp'] = datetime.utcnow().isoformat()
-        
-        # "Hive Partitioning"
-        date_part = datetime.utcnow().strftime('%Y-%m-%d')
-        file_name = f"bronze/crypto/binance/ticker/date={date_part}/btc_{int(datetime.utcnow().timestamp())}.json"
+    response = requests.get(url)
+    data = response.json()
+    data['ingestion_timestamp'] = datetime.utcnow().isoformat()
+    
+    date_part = datetime.utcnow().strftime('%Y-%m-%d')
+    file_name = f"bronze/crypto/binance/ticker/date={date_part}/btc_{int(datetime.utcnow().timestamp())}.json"
 
-        # S3Hook > official Airflow tool for aws
-        s3_hook = S3Hook(aws_conn_id='aws_default')
-        
-        s3_hook.load_string(
-            string_data=json.dumps(data),
-            key=file_name,
-            bucket_name=BUCKET_NAME,
-            replace=True
-        )
-        
-        logging.info(f"✅ SAVED FILE://{BUCKET_NAME}/{file_name}")
+    s3_hook = S3Hook(aws_conn_id='aws_default')
+    s3_hook.load_string(json.dumps(data), file_name, BUCKET_NAME, replace=True)
+    
+    logging.info(f"✅ Raw guardado: {file_name}")
+    
+    return file_name
 
-    except Exception as e:
-        logging.error(f"❌ ERROR: {e}")
-        raise e
+# transform new !
+def transform_to_silver(**context):
+
+    #transforms bronze data to Parquet 
+
+    source_key = context['task_instance'].xcom_pull(task_ids='extract_bronze')
+    logging.info(f"🔄 Procesando archivo: {source_key}")
+    
+    s3_hook = S3Hook(aws_conn_id='aws_default')
+    
+    # read from s3
+    file_content = s3_hook.read_key(source_key, BUCKET_NAME)
+    json_content = json.loads(file_content)
+    
+    # using pandas
+    df = pd.DataFrame([json_content]) # 1 row table
+    
+    # cleaning
+    df['price'] = df['price'].astype(float)
+    df['symbol'] = df['symbol'].astype(str)
+    df['ingestion_timestamp'] = pd.to_datetime(df['ingestion_timestamp'])
+    
+    # parquet
+    parquet_buffer = io.BytesIO()
+    df.to_parquet(parquet_buffer, index=False)
+    
+    # new silver "key"
+    dest_key = source_key.replace("bronze", "silver").replace(".json", ".parquet")
+    
+    # upload to s3
+    s3_hook.load_bytes(
+        parquet_buffer.getvalue(),
+        key=dest_key,
+        bucket_name=BUCKET_NAME,
+        replace=True
+    )
+    
+    logging.info(f"✨ Silver !: {dest_key}")
 
 with DAG(
-    dag_id='02_bitcoin_to_s3_bronze', # new one
+    dag_id='03_btc_etl_bronze_silver',
     default_args=default_args,
     schedule_interval='@daily',
     catchup=False,
-    tags=['ingest', 's3', 'bronze']
+    tags=['etl', 'crypto', 'bronze', 'silver']
 ) as dag:
 
-    ingest_task = PythonOperator(
-        task_id='fetch_and_save_btc',
-        python_callable=fetch_and_save_bitcoin
+    # Task 1 : ingest
+    extract_task = PythonOperator(
+        task_id='extract_bronze',
+        python_callable=fetch_bitcoin_raw,
+        provide_context=True
     )
+
+    # Task 2: transformation
+    transform_task = PythonOperator(
+        task_id='transform_silver',
+        python_callable=transform_to_silver,
+        provide_context=True
+    )
+
+    # define order, first extract and then transform
+    extract_task >> transform_task
